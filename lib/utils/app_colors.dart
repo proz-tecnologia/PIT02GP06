f805c87643028a6890f0207633afc9912b2d3499
leandro-mandu <leandro.om15@gmail.com>
2022-10-14 13:57:07 -0300

    criação da classe AppColors

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+//Classe para definir as cores básicas do projeto
+
+class AppColors {
+  static Color get textColor => Colors.white70;
+  static Color get titleColor => const Color(0xFF47525E);
+  static Color get backgroundColor => const Color(0xFFF5F6FF);
+  static Color get backgrountCardColor => const Color(0xFFFFFFFF);
+  static Color get backgrountCreditCardColor => const Color(0xFF13239F);
+  static Color get textCreditCardColor => const Color(0xFFF8F8F8);
+  static Color get backgrountButtonColor => const Color(0xFF13239F);
+  static Color get textButtonColor => const Color(0xFF242424);
+}
