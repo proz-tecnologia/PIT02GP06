1e0bd077fc95f0d8442fccbfd5034ba474c32d74
Julia <juliamarques61@gmail.com>
2022-10-20 16:48:09 -0300

    Classe AppColors ajustado

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
-//Classe para definir as cores básicas do projeto
+// Classe para definir as cores básicas do projeto
 
 class AppColors {
-  static Color get textColor => const Color(0xFF47525E);
-  static Color get titleColor => const Color(0xFF47525E);
+  // Texts
+  static Color get textIntroductionColor => const Color(0xFF242424);
+  static Color get textBodyColor => const Color(0xFF47525E);
+  static Color get textTitleColor => const Color(0xFF47525E);
+  static Color get textCreditCardColor => const Color(0xFFF8F8F8);
+  static Color get textButtonColor => const Color(0xFF242424);
+  static Color get textDropDown => const Color(0xFF242424);
+  static Color get textValueIncome => const Color(0xFF1E38FA);
+  static Color get textValueExpense => const Color(0xFFDB353E);
+
+  // Backgrounds
   static Color get backgroundColor => const Color(0xFFF5F6FF);
   static Color get backgroundCardColor => const Color(0xFFFFFFFF);
   static Color get backgroundCreditCardColor => const Color(0xFF13239F);
-  static Color get textCreditCardColor => const Color(0xFFF8F8F8);
   static Color get backgroundButtonBottomNavColor => const Color(0xFF242424);
-  static Color get textButtonColor => const Color(0xFF242424);
 }
