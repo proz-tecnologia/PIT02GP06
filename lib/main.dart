d58841c0076e07d9dff0d1a66a4cfa26c349468a
leandro-mandu <leandro.om15@gmail.com>
2022-10-20 18:11:19 -0300

    limpeza da função main e diminuição da AppColors

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pit02gp06/utils/app_colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,53 +15,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Projeto Integrador Grupo 6'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Center(
+        child: Container(
+          color: AppColors.backgroundColor,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
--- lib/utils/app_text_styles.dart
+++ lib/utils/app_text_styles.dart
@@ -3,21 +3,21 @@ import 'package:pit02gp06/utils/app_colors.dart';
 
 class AppTextStyles {
   static TextStyle get textHello =>
-      TextStyle(fontSize: 40, color: AppColors.textIntroductionColor);
+      TextStyle(fontSize: 40, color: AppColors.primaryColor);
   static TextStyle get textTitle =>
-      TextStyle(fontSize: 50, color: AppColors.textTitleColor);
+      TextStyle(fontSize: 50, color: AppColors.textColor);
   static TextStyle get textOfDropDown =>
-      TextStyle(fontSize: 50, color: AppColors.textDropDown);
+      TextStyle(fontSize: 50, color: AppColors.primaryColor);
   static TextStyle get textBalance =>
-      TextStyle(fontSize: 40, color: AppColors.textBodyColor);
+      TextStyle(fontSize: 40, color: AppColors.textColor);
   static TextStyle get textValueBalance =>
-      TextStyle(fontSize: 50, color: AppColors.textBodyColor);
+      TextStyle(fontSize: 50, color: AppColors.textColor);
   static TextStyle get textTitleIncomeAndExpense =>
-      TextStyle(fontSize: 50, color: AppColors.textBodyColor);
+      TextStyle(fontSize: 50, color: AppColors.textColor);
   static TextStyle get textValueIncome =>
       TextStyle(fontSize: 40, color: AppColors.textValueIncome);
   static TextStyle get textValueExpense =>
       TextStyle(fontSize: 40, color: AppColors.textValueExpense);
   static TextStyle get textNotRegistered =>
-      TextStyle(fontSize: 35, color: AppColors.textBodyColor);
+      TextStyle(fontSize: 35, color: AppColors.textColor);
 }
