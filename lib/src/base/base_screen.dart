f33d6d67daea05b3fac4a854461ca66da3b004d6
Julia <juliamarques61@gmail.com>
2022-10-26 14:55:32 -0300

    bottomNavBar estruturado, falta finalizar

--- lib/src/base/base_screen.dart
+++ lib/src/base/base_screen.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:pit02gp06/src/widgets/bottom_navigation_bar.dart';
 import 'package:pit02gp06/utils/app_colors.dart';
 
-class BaseScreen extends StatelessWidget {
+class BaseScreen extends StatefulWidget {
   const BaseScreen({super.key});
 
   @override
+  State<BaseScreen> createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(color: AppColors.backgroundColor),
-      bottomNavigationBar: ,
+      backgroundColor: AppColors.backgroundColor,
+      appBar: AppBar(
+        title: Text('BottomNavBar'),
+      ),
+      bottomNavigationBar: BottomNavBar(),
     );
   }
 }
--- lib/src/widgets/bottom_navigation_bar.dart
+++ lib/src/widgets/bottom_navigation_bar.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:pit02gp06/utils/app_colors.dart';
 
-class BottomNavigationBar extends StatelessWidget {
-  const BottomNavigationBar({super.key});
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
 
   @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    final Size size = MediaQuery.of(context).size;
+
+    return Container(
+      width: size.width,
+      height: 70,
+      color: AppColors.backgroundCardColor,
+      child: Stack(
+        children: [
+          CustomPaint(size: Size(size.width, 80)),
+          Center(
+            heightFactor: 0.5,
+            child: FloatingActionButton(
+              onPressed: () {},
+              backgroundColor: AppColors.backgroundButtonBottomNavColor,
+              child: Icon(Icons.add),
+            ),
+          ),
+          Container(
+            width: size.width,
+            height: 80,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(onPressed: () {}, icon: Icon(Icons.home)),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.add_business)),
+                  Container(
+                    width: size.width * 0.10,
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.corporate_fare_rounded)),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.account_circle_outlined)),
+                ]),
+          )
+        ],
+      ),
+    );
   }
 }
