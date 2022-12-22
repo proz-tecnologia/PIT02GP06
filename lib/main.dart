d7076809b58ec52b84dd0ce6699cc47e285d7e14
Victoria <victoriaellenov@gmail.com>
2022-12-21 22:15:45 -0300

    revert main

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        body: CategoryPage(),
+        body: BaseScreen(),
         backgroundColor: AppColors.backgroundColor,
       ),
     );
