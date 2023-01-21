ca1a6e15c22581c12e93f5aa9acd21048d0e7bb8
Julia <juliamarques61@gmail.com>
2023-01-21 18:35:35 -0300

    campo de telefone e gênero adicionado

--- lib/src/page/profile/profile_screen.dart
+++ lib/src/page/profile/profile_screen.dart
@@ -12,13 +12,19 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  DateTime? _dateTime = null;
+  DateTime? _dateTime;
+
+  // Image? imagePersonal;
 
   @override
   Widget build(BuildContext context) {
     final nameItem = TextFieldItem(
       label: 'Nome Completo',
     );
+    final phoneItem = TextFieldItem(label: 'Telefone');
+    const String url = "https://cdn-icons-png.flaticon.com/512/847/847969.png";
+    final dropValue = ValueNotifier('');
+    final dropOptions = ['Feminino', 'Masculino', 'Não informado'];
 
     return Scaffold(
       appBar: AppBar(
@@ -30,6 +36,25 @@ class _ProfileScreenState extends State<ProfileScreen> {
           child: Padding(
         padding: EdgeInsets.all(52.0),
         child: Column(children: [
+          // IMAGEM
+          Container(
+            height: 125,
+            width: 125,
+            decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: NetworkImage(url),
+                  fit: BoxFit.fitWidth,
+                )),
+          ),
+          Text(
+            'Nome Exemplo',
+            style: AppTextStyles.textNameProfileScreen,
+          ),
+          Text(
+            'email@exemplo.com.br',
+            style: AppTextStyles.textEmailProfileScreen,
+          ),
           Text(
             'Meus dados',
             style: AppTextStyles.textButtonSecondaryBlue,
@@ -38,6 +63,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
             item: nameItem,
           ),
           Container(
+            height: 55,
             decoration: BoxDecoration(
                 border: Border.all(width: 1),
                 borderRadius: BorderRadius.circular(12)),
@@ -63,7 +89,29 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     : _dateTime.toString()),
               ],
             ),
-          )
+          ),
+          TextFieldWidget(
+            item: phoneItem,
+          ),
+          ValueListenableBuilder(
+              valueListenable: dropValue,
+              builder: (BuildContext context, String value, _) {
+                return SizedBox(
+                  width: 280,
+                  child: DropdownButtonFormField<String>(
+                      isExpanded: true,
+                      hint: const Text('Não informado'),
+                      value: (value.isEmpty) ? null : value,
+                      onChanged: (choice) =>
+                          dropValue.value = choice.toString(),
+                      items: dropOptions
+                          .map((op) => DropdownMenuItem(
+                                value: op,
+                                child: Text(op),
+                              ))
+                          .toList()),
+                );
+              })
         ]),
       )),
     );
